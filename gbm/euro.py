6b3d2a26502802c8a3363e88ade5ae0edf67f09f
Steve Miles <steven.miles.quant@gmail.com>
2022-08-01 11:30:29 -0400

    to gbm.euro, added call valuation and made code object-oriented

--- gbm/euro.py
+++ gbm/euro.py
@@ -3,18 +3,24 @@ from scipy.stats import norm
 import pytest
 
 # Black-Scholes-Merton formula for a European put
-def euro_put_closed_form(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration):
+def black_scholes_merton(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration):
     sig_sqrt_t = sigma * sqrt(time_to_expiration);
     d1 = log(spot_price/strike) + (risk_free_rate-yield_rate+sigma*sigma/2)*time_to_expiration
     d1 /= sig_sqrt_t
     d2 = d1 - sig_sqrt_t
-    final_option_price = strike * exp(-risk_free_rate*time_to_expiration) * norm.cdf(-d2)
-    final_option_price -= spot_price * exp(-yield_rate*time_to_expiration) * norm.cdf(-d1)
+    
+    if put_or_call.lower() == "put":
+        final_option_price = strike * exp(-risk_free_rate*time_to_expiration) * norm.cdf(-d2)
+        final_option_price -= spot_price * exp(-yield_rate*time_to_expiration) * norm.cdf(-d1)
+    else:
+        final_option_price = spot_price * exp(-yield_rate*time_to_expiration) * norm.cdf(d1)
+        final_option_price -= strike * exp(-risk_free_rate*time_to_expiration) * norm.cdf(d2)
+        
     return final_option_price
 
 # Binomial tree algorithm for European put
 # Not written optimally for Europeans, but instead as a stepping-stone to American pricing
-def euro_put_binomial(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, n_time_steps):
+def binomial(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, n_time_steps):
     dt = time_to_expiration / n_time_steps
     u = exp(sigma*sqrt(dt))
     u_2 = u * u
@@ -22,23 +28,23 @@ def euro_put_binomial(spot_price, strike, risk_free_rate, yield_rate, sigma, tim
     disc = exp(-risk_free_rate*dt)
     p_up = (exp((risk_free_rate-yield_rate)*dt) - d)/(u-d)
     p_dn = 1-p_up
+    is_put = (put_or_call.lower() == "put")
 
     # Generate vector of final underlying values and final option prices
     final_underlying = spot_price * (d ** n_time_steps)
     # underlying_values = [None] * (n_time_steps+1)
-    option_prices = [0] * (n_time_steps+1)
+    option_prices = [None] * (n_time_steps+1)
     for state_idx in range(n_time_steps+1):
-        if strike <= final_underlying:
-            break
         # underlying_values[state_idx] = final_underlying
-        option_prices[state_idx] = strike-final_underlying
-        final_underlying = final_underlying * u_2
+        if is_put:
+            option_prices[state_idx] = max(strike-final_underlying, 0)
+        else:
+            option_prices[state_idx] = max(final_underlying-strike, 0)
+        final_underlying *= u_2
 
     # Moving backwards in time, discount european options from the bottom up
     for time_idx in range(n_time_steps):
         for state_idx in range(n_time_steps-time_idx):
-            if option_prices[state_idx] == 0:
-                break
             option_prices[state_idx] = disc * (p_up * option_prices[state_idx+1] + p_dn * option_prices[state_idx])
             # underlying_values[state_idx] *= u
     final_option_price = option_prices[0]
@@ -48,19 +54,23 @@ def euro_put_binomial(spot_price, strike, risk_free_rate, yield_rate, sigma, tim
 
 # Monte Carlo pricing for a European put
 # pseudoRandomDraws should be size [n_draws]
-def euro_put_monte_carlo(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, pseudoRandomDraws):
+def monte_carlo(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, pseudoRandomDraws):
     n_draws = len(pseudoRandomDraws)
     b = risk_free_rate - yield_rate
     drift_term = (b-sigma * sigma / 2)*time_to_expiration
     sig_sqrt_t = sigma * sqrt(time_to_expiration)
     spot_adj = spot_price * exp(drift_term)
+    is_put = (put_or_call.lower() == "put")
 
     # Calculate final underlying values and option payouts
     underlying_values = [None] * n_draws
     option_prices = [None] * n_draws
     for draw_idx in range(n_draws):
         underlying_values[draw_idx] = spot_adj * exp(sig_sqrt_t * pseudoRandomDraws[draw_idx])
-        option_prices[draw_idx] = max(strike-underlying_values[draw_idx], 0)
+        if is_put:
+            option_prices[draw_idx] = max(strike-underlying_values[draw_idx], 0)
+        else:
+            option_prices[draw_idx] = max(underlying_values[draw_idx]-strike, 0)
 
     # Take mean of payouts and discount to time zero
     final_option_price = sum(option_prices) / n_draws
--- tests/test_gbm_euro.py
+++ tests/test_gbm_euro.py
@@ -1,49 +1,55 @@
 import pytest
-from gbm.euro import euro_put_closed_form, euro_put_binomial, euro_put_monte_carlo
+import gbm.euro
 from random import seed, gauss
 from math import exp
 
-def test_gbm_euro_closed_form():
-    # From Haug page 3
-    spot_price = 60
-    strike = 65
-    risk_free_rate = 0.08
-    yield_rate = 0
-    sigma = 0.3
-    time_to_expiration = 0.25
-    call_price = 2.1334
-
+# Verify closed form against book
+# From Haug p3
+@pytest.mark.parametrize(('spot_price', 'strike', 'risk_free_rate', 'yield_rate', 'sigma', 'time_to_expiration', 'call_price'), (
+    (60, 65, 0.08, 0, 0.3, 0.25, 2.1334),
+))
+def test_gbm_euro_closed_form(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, call_price):
     put_price = call_price - spot_price * exp(-yield_rate*time_to_expiration) + strike * exp(-risk_free_rate*time_to_expiration)
-    test_closed_form = euro_put_closed_form(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
-    rel_diff = abs(test_closed_form - put_price) / put_price
 
-    assert rel_diff < 1e-5
+    test_call_price = gbm.euro.black_scholes_merton('call', spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
+    rel_diff = abs(test_call_price - call_price) / call_price
+    assert rel_diff < 1e-4
+
+    test_put_price = gbm.euro.black_scholes_merton('put', spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
+    rel_diff = abs(test_put_price - put_price) / put_price
+    assert rel_diff < 1e-4
 
 # Use closed form to verify binomial
-@pytest.mark.parametrize(('spot_price', 'strike', 'risk_free_rate', 'yield_rate', 'sigma', 'time_to_expiration'), (
-    (60, 65, 0.08, 0.01, 0.2, 0.25),
-    (100, 120, 0.08, 0.01, 0.3, 1),
-    (100, 80, 0.08, 0.02, 0.33, 2),
+@pytest.mark.parametrize(('put_or_call', 'spot_price', 'strike', 'risk_free_rate', 'yield_rate', 'sigma', 'time_to_expiration'), (
+    ('put', 60, 65, 0.08, 0.01, 0.2, 0.25),
+    ('put', 100, 120, 0.08, 0.01, 0.3, 1),
+    ('put', 100, 80, 0.08, 0.02, 0.33, 2),
+    ('call', 60, 65, 0.08, 0.01, 0.2, 0.25),
+    ('call', 100, 120, 0.08, 0.01, 0.3, 1),
+    ('call', 100, 80, 0.08, 0.02, 0.33, 2),
 ))
-def test_gbm_euro_binomial(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration):
-    test_closed_form = euro_put_closed_form(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
-    test_binomial = euro_put_binomial(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, 5000)
+def test_gbm_euro_binomial(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration):
+    test_closed_form = gbm.euro.black_scholes_merton(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
+    test_binomial = gbm.euro.binomial(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, 5000)
     rel_diff = abs(test_binomial-test_closed_form)/test_closed_form
     assert rel_diff < 1e-4
 
 # Use closed form to verify Monte Carlo
-@pytest.mark.parametrize(('spot_price', 'strike', 'risk_free_rate', 'yield_rate', 'sigma', 'time_to_expiration'), (
-    (60, 65, 0.08, 0.01, 0.2, 0.25),
-    (100, 120, 0.08, 0.01, 0.3, 1),
-    (100, 80, 0.08, 0.02, 0.33, 2),
+@pytest.mark.parametrize(('put_or_call', 'spot_price', 'strike', 'risk_free_rate', 'yield_rate', 'sigma', 'time_to_expiration'), (
+    ('put', 60, 65, 0.08, 0.01, 0.2, 0.25),
+    ('put', 100, 120, 0.08, 0.01, 0.3, 1),
+    ('put', 100, 80, 0.08, 0.02, 0.33, 2),
+    ('call', 60, 65, 0.08, 0.01, 0.2, 0.25),
+    ('call', 100, 120, 0.08, 0.01, 0.3, 1),
+    ('call', 100, 80, 0.08, 0.02, 0.33, 2),
 ))
-def test_gbm_euro_monte_carlo(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration):
-    test_closed_form = euro_put_closed_form(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
+def test_gbm_euro_monte_carlo(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration):
+    test_closed_form = gbm.euro.black_scholes_merton(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
     pseudoRandomDraws = [None] * 1000000
     seed(12345)
     for draw_idx in range(len(pseudoRandomDraws)):
         pseudoRandomDraws[draw_idx] = gauss(0, 1)
-    test_monte_carlo = euro_put_monte_carlo(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, pseudoRandomDraws)
+    test_monte_carlo = gbm.euro.monte_carlo(put_or_call, spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, pseudoRandomDraws)
     rel_diff = abs(test_monte_carlo-test_closed_form)/test_closed_form
     assert rel_diff < 1e-3
 
