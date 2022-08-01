b27defc4e73d62e3db5c0d991012dd81de1cbdea
Steve Miles <steven.miles.quant@gmail.com>
2022-08-01 09:32:12 -0400

    added gbm binromial and monte carlo

--- gbm/euro.py
+++ gbm/euro.py
@@ -2,45 +2,69 @@ from math import log, sqrt, exp
 from scipy.stats import norm
 import pytest
 
-# Black-Scholes-Merton formula for a European option
-def euro_put(CurrentUnderlyingPrice, StrikePrice, RiskFreeRate, YieldRate, Sigma, TimeToExpiration):
-    sigSqrtT = Sigma * sqrt(TimeToExpiration);
-    d1 = log(CurrentUnderlyingPrice/StrikePrice) + (RiskFreeRate-YieldRate+Sigma*Sigma/2)*TimeToExpiration
-    d1 /= sigSqrtT
-    d2 = d1 - sigSqrtT
-    euroOptionPrice = StrikePrice * exp(-RiskFreeRate*TimeToExpiration) * norm.cdf(-d2)
-    euroOptionPrice -= CurrentUnderlyingPrice * exp(-YieldRate*TimeToExpiration) * norm.cdf(-d1)
-    return euroOptionPrice
+# Black-Scholes-Merton formula for a European put
+def euro_put_closed_form(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration):
+    sig_sqrt_t = sigma * sqrt(time_to_expiration);
+    d1 = log(spot_price/strike) + (risk_free_rate-yield_rate+sigma*sigma/2)*time_to_expiration
+    d1 /= sig_sqrt_t
+    d2 = d1 - sig_sqrt_t
+    final_option_price = strike * exp(-risk_free_rate*time_to_expiration) * norm.cdf(-d2)
+    final_option_price -= spot_price * exp(-yield_rate*time_to_expiration) * norm.cdf(-d1)
+    return final_option_price
 
-# Binomial tree algorithm for European option
+# Binomial tree algorithm for European put
 # Not written optimally for Europeans, but instead as a stepping-stone to American pricing
-def euro_put_binomial(CurrentUnderlyingPrice, StrikePrice, RiskFreeRate, YieldRate, Sigma, TimeToExpiration, nTimeSteps):
-    dt = TimeToExpiration / nTimeSteps
-    u = exp(Sigma*sqrt(dt))
-    uSq = u * u
+def euro_put_binomial(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, n_time_steps):
+    dt = time_to_expiration / n_time_steps
+    u = exp(sigma*sqrt(dt))
+    u_2 = u * u
     d = 1/u
-    disc = exp(-RiskFreeRate*dt)
-    pUp = (exp((RiskFreeRate-YieldRate)*dt) - d)/(u-d)
-    pDn = 1-pUp
+    disc = exp(-risk_free_rate*dt)
+    p_up = (exp((risk_free_rate-yield_rate)*dt) - d)/(u-d)
+    p_dn = 1-p_up
 
     # Generate vector of final underlying values and final option prices
-    finalUnderlying = CurrentUnderlyingPrice * (d ** nTimeSteps)
-    # underlyingValues = [None] * (nTimeSteps+1)
-    optionPrices = [0] * (nTimeSteps+1)
-    for stateIdx in range(nTimeSteps+1):
-        if StrikePrice <= finalUnderlying:
+    final_underlying = spot_price * (d ** n_time_steps)
+    # underlying_values = [None] * (n_time_steps+1)
+    option_prices = [0] * (n_time_steps+1)
+    for state_idx in range(n_time_steps+1):
+        if strike <= final_underlying:
             break
-        # underlyingValues[stateIdx] = finalUnderlying
-        optionPrices[stateIdx] = StrikePrice-finalUnderlying
-        finalUnderlying = finalUnderlying * uSq
+        # underlying_values[state_idx] = final_underlying
+        option_prices[state_idx] = strike-final_underlying
+        final_underlying = final_underlying * u_2
 
     # Moving backwards in time, discount european options from the bottom up
-    for timeIdx in range(nTimeSteps):
-        for stateIdx in range(nTimeSteps-timeIdx):
-            if optionPrices[stateIdx] == 0:
+    for time_idx in range(n_time_steps):
+        for state_idx in range(n_time_steps-time_idx):
+            if option_prices[state_idx] == 0:
                 break
-            optionPrices[stateIdx] = disc * (pUp * optionPrices[stateIdx+1] + pDn * optionPrices[stateIdx])
-            # underlyingValues[stateIdx] *= u
+            option_prices[state_idx] = disc * (p_up * option_prices[state_idx+1] + p_dn * option_prices[state_idx])
+            # underlying_values[state_idx] *= u
+    final_option_price = option_prices[0]
 
-    return optionPrices[0]
+    return final_option_price
+
+
+# Monte Carlo pricing for a European put
+# pseudoRandomDraws should be size [n_draws]
+def euro_put_monte_carlo(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration, pseudoRandomDraws):
+    n_draws = len(pseudoRandomDraws)
+    b = risk_free_rate - yield_rate
+    drift_term = (b-sigma * sigma / 2)*time_to_expiration
+    sig_sqrt_t = sigma * sqrt(time_to_expiration)
+    spot_adj = spot_price * exp(drift_term)
+
+    # Calculate final underlying values and option payouts
+    underlying_values = [None] * n_draws
+    option_prices = [None] * n_draws
+    for draw_idx in range(n_draws):
+        underlying_values[draw_idx] = spot_adj * exp(sig_sqrt_t * pseudoRandomDraws[draw_idx])
+        option_prices[draw_idx] = max(strike-underlying_values[draw_idx], 0)
+
+    # Take mean of payouts and discount to time zero
+    final_option_price = sum(option_prices) / n_draws
+    final_option_price *= exp(-risk_free_rate*time_to_expiration)
+
+    return final_option_price
 
--- tests/test_gbm_euro.py
+++ tests/test_gbm_euro.py
@@ -1,15 +1,39 @@
 import pytest
-from gbm.euro import euro_put, euro_put_binomial
+from gbm.euro import euro_put_closed_form, euro_put_binomial, euro_put_monte_carlo
+from random import seed, gauss
+from math import exp
 
-def test_gbm_euro():
-    # From Haug p3
-    test_Haug_p3 = euro_put(60, 65, 0.08, 0, 0.2, 0.25)
-    assert test_Haug_p3 == 4.754368137482118
+def test_gbm_euro_closed_form():
+    # From Haug page 3
+    spot_price = 60
+    strike = 65
+    risk_free_rate = 0.08
+    yield_rate = 0
+    sigma = 0.3
+    time_to_expiration = 0.25
+    call_price = 2.1334
 
-    test_closedform = euro_put(60, 65, 0.08, 0.01, 0.2, 0.25)
+    put_price = call_price - spot_price * exp(-yield_rate*time_to_expiration) + strike * exp(-risk_free_rate*time_to_expiration)
+    test_closed_form = euro_put_closed_form(spot_price, strike, risk_free_rate, yield_rate, sigma, time_to_expiration)
+    rel_diff = abs(test_closed_form - put_price) / put_price
+
+    assert rel_diff < 1e-5
+
+# Use closed form to verify binomial
+def test_gbm_euro_binomial():
+    test_closed_form = euro_put_closed_form(60, 65, 0.08, 0.01, 0.2, 0.25)
     test_binomial = euro_put_binomial(60, 65, 0.08, 0.01, 0.2, 0.25, 1000)
-    relDiff = abs(test_binomial-test_closedform)/test_closedform
-    assert relDiff < 1e-4
+    rel_diff = abs(test_binomial-test_closed_form)/test_closed_form
+    assert rel_diff < 1e-4
 
-
+# Use closed form to verify Monte Carlo
+def test_gbm_euro_monte_carlo():
+    test_closed_form = euro_put_closed_form(60, 65, 0.08, 0.01, 0.2, 0.25)
+    pseudoRandomDraws = [None] * 100000
+    seed(12345)
+    for draw_idx in range(len(pseudoRandomDraws)):
+        pseudoRandomDraws[draw_idx] = gauss(0, 1)
+    test_monte_carlo = euro_put_monte_carlo(60, 65, 0.08, 0.01, 0.2, 0.25, pseudoRandomDraws)
+    rel_diff = abs(test_monte_carlo-test_closed_form)/test_closed_form
+    assert rel_diff < 1e-3
 
