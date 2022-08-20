6c7bccea8653ec9d83f28c4d2fa7dbf4add20a82
Steve Miles <steven.miles.quant@gmail.com>
2022-08-20 14:49:19 -0400

    missed an update for test_uit.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy
-from util.num import tridiag_solve, tridiag_inv
+from util import tridiag_solve
+
 
 # Test tridiag_solve
 def test_util_tridiag_solve():
@@ -22,30 +23,4 @@ def test_util_tridiag_solve():
         if i < n-1:
             sum += c[i] * x[i+1]
         assert abs(sum - d[i]) < 1e-8
-
-
-# Test tridiag_inv
-def test_util_tridiag_inv():
-    n = 4
-    a = numpy.full(n, 1) # index 0 is ignored
-    b = numpy.full(n, -2.6)
-    c = numpy.full(n-1, 1)
-
-    M = tridiag_inv(a, b, c)
-
-    L = numpy.zeros((n, n))
-    for i in range(n):
-        if i > 0:
-            L[i][i-1] = a[i]
-        L[i][i] = b[i]
-        if i < n-1:
-            L[i][i+1] = c[i]
-
-    I = numpy.matmul(L, M)
-
-    for i in range(n):
-        assert abs(I[i,i] - 1) < 1e-8
-        for j in range(i):
-            assert abs(I[i,j]) < 1e-8
-            assert abs(I[j,i]) < 1e-8
 
